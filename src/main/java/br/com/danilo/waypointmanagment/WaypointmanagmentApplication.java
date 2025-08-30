bb877eec1650e2f6b315b5da22f1cf427d589aa4
Danilo Alves <danilo.alvess1902@gmail.com>
2025-08-29 23:16:49 -0300

    Initial commit

---
+++ src/main/java/br/com/danilo/waypointmanagment/WaypointmanagmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.danilo.waypointmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaypointmanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaypointmanagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danilo/waypointmanagment/WaypointmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danilo.waypointmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaypointmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
