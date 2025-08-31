f96a9302abb60c447519bee5433286bf1b21c8b2
Danilo Alves <danilo.alvess1902@gmail.com>
2025-08-31 10:55:16 -0300

    feat: add waypoint gateway

---
+++ src/main/java/br/com/danilo/waypointmanagment/application/gateway/WaypointGateway.java
@@ -0,0 +1,6 @@
+package br.com.danilo.waypointmanagment.application.gateway;
+
+import br.com.danilo.waypointmanagment.domain.entity.Waypoint;
+
+public interface WaypointGateway extends Gateway<Waypoint> {
+}
